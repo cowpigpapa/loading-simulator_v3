328fb9e355da04de3ebe4f9af0b097b445401cbe
cowpigpapa <cowpigpapa@users.noreply.github.com>
2026-08-11 13:46:46 +0900

    Connect LoadWise v3 to Supabase Auth

--- supabase-config.js
+++ supabase-config.js
@@ -1,1 +1,4 @@
-window.LOADWISE_SUPABASE = { url: '', publishableKey: '' };
+window.LOADWISE_SUPABASE = {
+  url: 'https://wejegdshcpqqqhhjbtox.supabase.co',
+  publishableKey: 'sb_publishable_nUwCSEDHdD5sgQAooaJwew_g8hL0r0C'
+};
